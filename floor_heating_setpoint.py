2418d6e9ce473649212ed5542867ebca16c1124b
dededemio <demio@vivaldi.net>
2024-01-10 05:39:42 +0900

    玄関ドア、土間、浴槽、換気熱損失を追加。複数部屋の計算に対応

--- floor_heating_setpoint.py
+++ floor_heating_setpoint.py
@@ -4,68 +4,70 @@ import pandas as pd
 from scipy.optimize import minimize
 from functools import partial
 from matplotlib import pyplot as plt
+plt.rcParams['font.family'] = "Yu Gothic"
+
+HOME_PARAM_EXCEL = pd.ExcelFile("home_parameter.xlsx")
+ROOM_PARAM = HOME_PARAM_EXCEL.parse("room_param", index_col=0)
+HEAT_TRANSFER_COEF = HOME_PARAM_EXCEL.parse("heat_transfer_coef", index_col=0)
+HOME_PARAM_EXCEL.close()
+SENSIBLE_HEAT_EX_EFFICIENCY = 0.9
 
 # 室の熱損失と床暖からの熱取得の差を計算
-def calc_heat_diff(outdoor_temp, heating_setpoint, room_temp):
-    # 部屋毎に変更するパラメータ
-    wall_area_total = 35.1 # 窓含む壁の面積[m2]
-    window_area_case = 2.1  # 開き窓の面積[m2]
-    window_area_slide = 5.4  # 引き違い窓の面積[m2]
-    wall_area = wall_area_total - window_area_case - window_area_slide
-    ceiling_area = 0 # 天井の面積[m2]
-    floor_heating_area = 21.8  # 床暖房の面積[m2]
+def calc_heat_diff(room, outdoor_temp, heating_setpoint, room_temp):
+    # 壁、窓、天井、玄関から室外への熱損失(床は床暖房で温められるため考慮しない)
+    heat_loss_envelope = (ROOM_PARAM[room]["窓・ドア除く壁面積[㎡]"] * HEAT_TRANSFER_COEF["熱貫流率"]["壁"] + 
+                         ROOM_PARAM[room]["開き窓面積[㎡]"] * HEAT_TRANSFER_COEF["熱貫流率"]["開き窓"] + 
+                         ROOM_PARAM[room]["引き違い窓面積[㎡]"] * HEAT_TRANSFER_COEF["熱貫流率"]["引き違い窓"] + 
+                         ROOM_PARAM[room]["天井面積[㎡]"] * HEAT_TRANSFER_COEF["熱貫流率"]["天井"] +
+                         ROOM_PARAM[room]["玄関ドア面積[㎡]"] * HEAT_TRANSFER_COEF["熱貫流率"]["玄関ドア"] +
+                         ROOM_PARAM[room]["玄関土間外周長さ[m]"] * HEAT_TRANSFER_COEF["熱貫流率"]["玄関土間"] + 
+                         ROOM_PARAM[room]["浴槽面積[㎡]"] * HEAT_TRANSFER_COEF["熱貫流率"]["浴槽"] * 0.6) * (room_temp - outdoor_temp)
 
-    # 建物で固定のパラメータ
-    wall_heat_transfer_coeff = 0.24  # 壁の熱貫流率[W/m2K]
-    window_heat_transfer_coeff_case = 0.8  # 開き窓の熱貫流率[W/m2K]
-    window_heat_transfer_coeff_slide = 1.0  # 引き違い窓の熱貫流率[W/m2K]
-    ceiling_heat_transfer_coeff = 0.15  # 天井の熱貫流率[W/m2K]
-    floor_heating_transfer_coeff = 2.7 # 床暖房パイプからの熱貫流率[W/m2K]
+    # 換気による熱損失
+    heat_loss_ventilation = 0.33 * (ROOM_PARAM[room]["換気量[㎥/h]"]) * (room_temp - outdoor_temp) * SENSIBLE_HEAT_EX_EFFICIENCY
 
-    # 壁、窓、天井から室外への熱損失
-    heat_loss_outside = (wall_area * wall_heat_transfer_coeff + window_area_case * window_heat_transfer_coeff_case + 
-                        window_area_slide * window_heat_transfer_coeff_slide + 
-                        ceiling_area * ceiling_heat_transfer_coeff) * (room_temp - outdoor_temp)
+    # 内部熱負荷
+    heat_supply_internal = ROOM_PARAM[room]["内部熱負荷[W]"]
 
-    # 床暖房からの熱供給。室温のほうが床暖設定より高い場合は0とする
-    heat_supply_floor_heating = floor_heating_area * floor_heating_transfer_coeff * np.maximum((heating_setpoint - room_temp),0)
+    # 床暖房からの熱取得（室温のほうが床暖設定より高い場合は0）
+    heat_supply_floor_heating = ROOM_PARAM[room]["床暖房面積[㎡]"] * HEAT_TRANSFER_COEF["熱貫流率"]["床暖房"] * np.maximum((heating_setpoint - room_temp),0)
     
-    # 室内の総熱量
-    heat_diff = abs(heat_supply_floor_heating - heat_loss_outside)
+    # 室の熱損失と熱取得の差の絶対値
+    heat_diff = abs(heat_supply_floor_heating - heat_loss_envelope - heat_loss_ventilation + heat_supply_internal)
 
     return heat_diff
 
 # ある外気温、床暖設定のときの室温を計算する
-def calc_room_temperature(outdoor_temp, heating_setpoint):
-    objective_func = partial(calc_heat_diff, outdoor_temp, heating_setpoint)
+def calc_room_temperature(room, outdoor_temp, heating_setpoint):
+    objective_func = partial(calc_heat_diff, room=room, outdoor_temp=outdoor_temp, heating_setpoint=heating_setpoint)
     initial_guess = [outdoor_temp] # 室温の初期値
-    result = minimize(objective_func, initial_guess, method="Nelder-Mead")
+    result = minimize(lambda x: objective_func(room_temp=x), initial_guess, method="Nelder-Mead")
     return result.x[0]
 
 # 目標室温にするための床暖設定温度を求める
-def calc_heating_setpoint(room_setpoint, outdoor_temp):
-    objective_func = partial(calc_heat_diff, outdoor_temp=outdoor_temp, room_temp=room_setpoint)
+def calc_heating_setpoint(room, room_setpoint, outdoor_temp):
+    objective_func = partial(calc_heat_diff, room=room, outdoor_temp=outdoor_temp, room_temp=room_setpoint)
     initial_guess = [room_setpoint] # 床暖設定の初期値
     result = minimize(lambda x: objective_func(heating_setpoint=x), initial_guess, method="Nelder-Mead")
     return np.ceil(result.x[0])
 
 # 外気温別の床暖設定をグラフ化
-room_setpoint = 23 # 目標室温
 outdoor_temp = np.round(np.arange(-5.0, 15.0, 1), 1)
-heating_setpoint = [calc_heating_setpoint(room_setpoint, x) for x in outdoor_temp]
-heating_setpoint_list = pd.DataFrame(heating_setpoint, index=outdoor_temp, columns=["床暖設定温度[℃]"])
-heating_setpoint_list.index.name="外気温[℃]"
-print(heating_setpoint_list)
+for room in ROOM_PARAM.columns:
+    room_setpoint = ROOM_PARAM[room]["目標温度[℃]"]
+    heating_setpoint = [calc_heating_setpoint(room, room_setpoint, x) for x in outdoor_temp]
+    heating_setpoint_list = pd.DataFrame(heating_setpoint, index=outdoor_temp, columns=["床暖設定温度[℃]"])
+    heating_setpoint_list.index.name="外気温[℃]"
+    print(heating_setpoint_list)
 
-# グラフ描画
-plt.plot(heating_setpoint_list)
-plt.grid()
-plt.xlabel("Outdoor temperature[℃]")
-plt.ylabel("Floor heating setpoint[℃]")
+    # グラフ描画
+    plt.plot(heating_setpoint_list)
+    plt.grid()
+    plt.xlabel("外気温[℃]")
+    plt.ylabel("床暖房設定温度目安[℃]")
+plt.legend(ROOM_PARAM.columns)
 
-
-
-# 2022年の気温から、夜間最低温度と昼平均温度を計算して、推奨設定を計算
+# 2022年の気温から、夜間最低温度と昼平均温度を計算
 # 夜間は18:00～9:00とする
 hour_start_night = 18
 hour_end_night = 9
@@ -74,7 +76,6 @@ temp_act = pd.read_csv("data.csv", header=2, index_col=0, encoding="shift-jis")
 temp_act.index = pd.to_datetime(temp_act.index)
 # 10月～4月を対象に計算
 month = [10, 11, 12, 1, 2, 3, 4]
-setpoint = pd.DataFrame(columns=["通常温度", "セーブ温度"])
 temp_min_avg = pd.DataFrame(columns=["夜間最低気温", "昼間平均気温"])
 for m in month:
     # 月別の昼・夜間データを抽出
@@ -83,11 +84,42 @@ for m in month:
     temp_day = temp_month[(temp_month.index.hour < hour_start_night) & (temp_month.index.hour >= hour_end_night)]
     # 夜間最低温度、昼間平均気温を格納
     temp_min_avg.loc[m] = [temp_night.min()[0], round(temp_day.mean()[0],1)]
-    # 目標室温を保つ設定温度を計算して格納
-    setpoint_night = calc_heating_setpoint(room_setpoint, temp_night.min()[0])
-    setpoint_day = calc_heating_setpoint(room_setpoint, temp_day.mean()[0])
-    setpoint.loc[m] = [setpoint_night, setpoint_day]
-
-# 計算結果を表示
 print(temp_min_avg)
-print(setpoint)
+
+# 各部屋の推奨設定を計算
+setpoint_total = []
+for m in month:
+    setpoint = pd.DataFrame(columns=["通常温度", "セーブ温度"])
+    for room in ROOM_PARAM.columns:
+        # 目標室温を保つ設定温度を計算して格納
+        room_setpoint = ROOM_PARAM[room]["目標温度[℃]"]
+        setpoint_night = calc_heating_setpoint(room, room_setpoint, temp_min_avg["夜間最低気温"][m])
+        setpoint_day = calc_heating_setpoint(room, room_setpoint, temp_min_avg["昼間平均気温"][m])
+        setpoint.loc[room] = [setpoint_night, setpoint_day]
+    setpoint_total.append(setpoint)
+
+for m, setpoint in zip(month, setpoint_total):
+    print(str(m)+"月", setpoint)
+
+# 実際の計測温度との比較----------------------------------------------------
+outdoor_temp = np.round(np.arange(-5.0, 15.0, 0.1), 1)
+heating_setpoint_list = np.arange(26, 31)
+room_temp_total = []
+for heating_setpoint in heating_setpoint_list:
+    room_temp = [calc_room_temperature("リビング", x, heating_setpoint) for x in outdoor_temp]
+    room_temp_total.append(room_temp)
+actual = pd.read_csv("actual.csv", encoding="shift-jis")
+
+for i in range(len(heating_setpoint_list)):
+    plt.plot(outdoor_temp, room_temp_total[i])
+for heating_setpoint in heating_setpoint_list:
+    picup = actual[actual["設定温度"]==heating_setpoint]
+    plt.plot(picup["最低気温"], picup["室温"], "*")
+plt.grid()
+str_list_predict = ["設定"+str(i)+"℃(予想)" for i in heating_setpoint_list]
+str_list_actual = ["設定"+str(i)+"℃(実績)" for i in heating_setpoint_list]
+plt.title("設定温度別の予想室温と実績")
+plt.legend(str_list_predict+str_list_actual)
+plt.ylim([19.8, 27.5])
+plt.xlabel("外気温[℃]")
+plt.ylabel("室温[℃]")
